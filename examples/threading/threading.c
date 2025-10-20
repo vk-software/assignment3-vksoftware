0ff5317ee4f4b00fcbd21c89a2d01a1bb28595cf
vk-software <valeross@ukr.net>
2025-10-20 13:20:11 +0000

    Implement Assignment 4 Part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,30 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    if (usleep(thread_func_args->wait_to_obtain_ms * 1000) != 0) {
+        DEBUG_LOG("Failed to sleep!\n");
+        thread_func_args->thread_complete_success = false;
+    }
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    
+    if (rc != 0) {
+        DEBUG_LOG("Mutex lock failed!\n");
+        thread_func_args->thread_complete_success = false;
+    }
+    else {
+        if (usleep(thread_func_args->wait_to_release_ms * 1000) != 0) {
+            DEBUG_LOG("Failed to sleep!\n");
+            thread_func_args->thread_complete_success = false;
+        }
+        if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+            DEBUG_LOG("Failed to unlock mutex!");
+            thread_func_args->thread_complete_success = false;
+        }
+    }
     return thread_param;
 }
 
@@ -28,6 +52,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    struct thread_data* thread_func_args = (struct thread_data *)malloc(sizeof(struct thread_data));
+    
+    thread_func_args->mutex = mutex;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+    thread_func_args->thread_complete_success = true;
+    
+    if (pthread_create(thread, NULL, threadfunc, (void *)thread_func_args) != 0) {
+        printf("Failed to create a thread!\n\n");
+        return false;
+    }
+
+    return thread_func_args->thread_complete_success;
 }
 
